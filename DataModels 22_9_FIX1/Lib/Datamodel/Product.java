f7854585d370bc8f30d1c75e6d5bbaf16c93a132
6721600978 Kasidet <kasidet.lee@ku.th>
2025-09-22 20:01:48 +0700

    Delete DataModels 22_9_FIX1 directory

--- DataModels 22_9_FIX1/Lib/Datamodel/Product.java
+++
@@ -1,7 +0,0 @@
-package Lib.Datamodel;
-
-public record Product(String sku,String name,double price,int stock) {
-
-    // sku = รหัสสินค้า , name = ชื่อสินค้า , price = ราคาต่อหน่วย , stock = จำนวนสินค้าในคลัง
-    
-}
--- DataModels 22_9_FIX1/Product.java
+++
@@ -1,7 +0,0 @@
-package DataModels;
-
-public record Product(String sku,String name,double price,int stock) {
-
-    // sku = รหัสสินค้า , name = ชื่อสินค้า , price = ราคาต่อหน่วย , stock = จำนวนสินค้าในคลัง
-    
-}
