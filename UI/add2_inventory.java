6b1284cabc679bf2771899f5f65cc007c24c39b5
6721600978 Kasidet <kasidet.lee@ku.th>
2025-10-13 02:11:51 +0700

    Add files via upload

---
+++ UI/add2_inventory.java
@@ -0,0 +1,138 @@
+package UI;
+import javax.swing.*;
+
+import DataModels.Product;
+import Services.InventoryService;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class add2_inventory extends JFrame implements ActionListener{
+    Container cp;
+    JButton home,add;
+    JTextField sku,quantity,name,price;
+    JLabel sku_t,quantity_t,name_t,price_t;
+    private final InventoryService inventoryService; 
+    private final inventory parentFrame;
+    ImageIcon home_pic=new ImageIcon("./picture/home.png");
+    public add2_inventory(InventoryService inventoryService,inventory parentFrame){
+      super("MR.DRY");
+      this.parentFrame = parentFrame;
+      this.inventoryService = inventoryService; // รับ InventoryService เข้ามา
+      Initial();
+      setComponent();
+      Finally();
+    }
+    public void Initial(){
+    cp=getContentPane();
+    cp.setLayout(null);
+    cp.setBackground(new Color(216,191,216));
+    }
+    public void setComponent(){
+        home=new JButton();
+    home.setIcon(home_pic);
+    home.setBackground(new Color(216,191,216));
+    home.setSize(20, 20);
+    home.setBorderPainted(false);
+    home.setBounds(0,0,20,20);
+    sku=new JTextField();
+    sku.setBounds(50,50,200,25);
+    sku_t=new JLabel("SKU");
+    sku_t.setBounds(50, 20, 50,20);
+    sku_t.setBackground(new Color(216,191,216));
+    sku_t.setForeground(new Color(250,248,228));
+    sku_t.setFont(new Font("Garamond",Font.BOLD, 18));
+    name=new JTextField();
+    name.setBounds(50,120,200,25);
+    name_t=new JLabel("Name");
+    name_t.setBounds(50, 90, 100,20);
+    name_t.setBackground(new Color(216,191,216));
+    name_t.setForeground(new Color(250,248,228));
+    name_t.setFont(new Font("Garamond",Font.BOLD, 18));
+    price=new JTextField();
+    price.setBounds(50,190,200,25);
+    price_t=new JLabel("Price");
+    price_t.setBounds(50, 160, 100,20);
+    price_t.setBackground(new Color(216,191,216));
+    price_t.setForeground(new Color(250,248,228));
+    price_t.setFont(new Font("Garamond",Font.BOLD, 18));
+    quantity=new JTextField();
+    quantity.setBounds(50,260,200,25);
+    quantity_t=new JLabel("Quanlity");
+    quantity_t.setBounds(50, 230, 100,20);
+    quantity_t.setBackground(new Color(216,191,216));
+    quantity_t.setForeground(new Color(250,248,228));
+    quantity_t.setFont(new Font("Garamond",Font.BOLD, 18));
+    add=new JButton("Add");
+    add.setBounds(50, 300, 90,40);
+    add.setBackground(new Color(250,250,250));
+    add.setForeground(new Color(216,191,216));
+    add.setFont(new Font("Garamond",Font.BOLD, 28));
+    add.setBorderPainted(false);
+
+    cp.add(name);
+    cp.add(name_t);
+    cp.add(price);
+    cp.add(price_t);
+    cp.add(quantity_t);
+    cp.add(sku_t);
+    cp.add(sku);
+    cp.add(home);
+    cp.add(quantity);
+    cp.add(add);
+    home.addActionListener(this);
+    add.addActionListener(this);
+    }
+    public void Finally(){
+    this.setSize(320,400);
+    this.setVisible(true);
+    this.setLocationRelativeTo(null);
+    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+ }
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      if (e.getSource() == add) {
+            String skuInput = sku.getText().trim();
+            String nameInput = name.getText().trim();
+            String priceInput = price.getText().trim();
+            String qtyInput = quantity.getText().trim();
+
+            try {
+               if (skuInput.isEmpty() || nameInput.isEmpty() || priceInput.isEmpty() || qtyInput.isEmpty()) {
+                  JOptionPane.showMessageDialog(this, "Please fill in all fields.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                  return;
+               }
+               
+               double newPrice = Double.parseDouble(priceInput);
+               int newStock = Integer.parseInt(qtyInput);
+               
+               if (newPrice < 0 || newStock < 0) {
+                     JOptionPane.showMessageDialog(this, "Price and Quantity must be non-negative numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                     return;
+               }
+
+                // เรียกใช้ addProduct ซึ่งจะบันทึกเข้า CSV ด้วย
+               Product newProduct = new Product(skuInput, nameInput, newPrice, newStock);
+               inventoryService.addProduct(newProduct);
+
+               JOptionPane.showMessageDialog(this, "Product '" + nameInput + "' added successfully and saved to CSV!", "Success", JOptionPane.INFORMATION_MESSAGE);
+               this.dispose();
+
+   
+                // เรียกเมทอดอัปเดตข้อมูลของหน้าจอหลัก
+                parentFrame.loadProductData(); 
+                
+                this.dispose();
+
+            } catch (NumberFormatException ex) {
+               JOptionPane.showMessageDialog(this, "Price and Quantity must be valid numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+            } catch (RuntimeException ex) {
+                // RuntimeException ถูกโยนเมื่อ SKU ซ้ำ
+               JOptionPane.showMessageDialog(this, "Error: " + ex.getMessage(), "Operation Error", JOptionPane.ERROR_MESSAGE);
+            }
+      }else if(e.getSource()==home){
+      new dashboard();
+      dispose();
+   }
+   }
+}
---
+++ UI/loginpanel.java
@@ -0,0 +1,166 @@
+package UI;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class loginpanel extends JFrame implements ActionListener{
+
+    private JTextField txtUser;
+    private JPasswordField txtPassword;
+    private JButton btnLogin;
+    Container cp;
+    public loginpanel() {
+            initComponents();
+    }
+
+    private void initComponents() {
+            cp=getContentPane();
+            cp.setLayout(null);
+            cp.setBackground(new Color(211, 222, 174));
+
+        // ----------------- LOGO -----------------
+            ImageIcon centerIcon = new ImageIcon("./picture/MRDRY.png");
+            Image centerImg = centerIcon.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
+            JLabel lblLogo = new JLabel(new ImageIcon(centerImg));
+            lblLogo.setBounds(150, 20, 80, 80);
+            cp.add(lblLogo);
+
+        // ----------------- Panel ด้านใน ----------------------
+            JPanel jPanel2 = new JPanel();
+            jPanel2.setBackground(Color.WHITE);
+            jPanel2.setLayout(null);
+            jPanel2.setBounds(50, 120, 280, 160);  
+            jPanel2.setBorder(BorderFactory.createLineBorder(new Color(180, 180, 180), 1));
+            cp.add(jPanel2);
+
+        // ------------------ USERNAME ----------------------
+            JLabel userIcon = new JLabel("👤");
+            userIcon.setBounds(20, 40, 25, 25);
+            jPanel2.add(userIcon);
+
+            JLabel l1 = new JLabel("USERNAME");
+            l1.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+            l1.setBounds(20, 10, 120, 20);
+            jPanel2.add(l1);
+
+        // -------------------- เคลียร์ USER  --------------------
+            JLayeredPane userPane = new JLayeredPane();
+            userPane.setBounds(50, 40, 200, 25);
+
+            txtUser = new JTextField();
+            txtUser.setBackground(new Color(245, 245, 245));
+            txtUser.setBounds(0, 0, 200, 25);
+            txtUser.setMargin(new Insets(0, 5, 0, 20)); // กันข้อความทับ ❌
+
+            JLabel clearUser = new JLabel("❌", SwingConstants.CENTER);
+            clearUser.setBounds(180, 0, 20, 25);
+            clearUser.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                txtUser.setText("");
+            }
+            });
+
+            userPane.add(txtUser, Integer.valueOf(0));
+            userPane.add(clearUser, Integer.valueOf(1));
+            jPanel2.add(userPane);
+
+        // -------------------- PASSWORD ------------------------
+            JLabel passIcon = new JLabel("🔒");
+            passIcon.setBounds(20, 100, 25, 25);
+            jPanel2.add(passIcon);
+
+            JLabel l2 = new JLabel("PASSWORD");
+            l2.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+            l2.setBounds(20, 70, 120, 20);
+            jPanel2.add(l2);
+
+        // -------------------- เคลียร์ PASSWORD  --------------------
+            JLayeredPane passPane = new JLayeredPane();
+            passPane.setBounds(50, 100, 200, 25);
+
+            txtPassword = new JPasswordField();
+            txtPassword.setBackground(new Color(245, 245, 245));
+            txtPassword.setBounds(0, 0, 200, 25);
+        
+            JLabel clearPass = new JLabel("❌", SwingConstants.CENTER);
+            clearPass.setBounds(180, 0, 20, 25);
+            clearPass.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                txtPassword.setText("");
+            }
+        });
+
+            passPane.add(txtPassword, Integer.valueOf(0));
+            passPane.add(clearPass, Integer.valueOf(1));
+            jPanel2.add(passPane);
+
+        // -------------------- ปุ่ม Login ---------------------
+        btnLogin = new JButton("LOGIN");
+            btnLogin.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+            btnLogin.setBounds(140, 300, 100, 30);
+            btnLogin.setBackground(new Color(242, 145, 129));
+            btnLogin.setForeground(Color.WHITE);
+            btnLogin.setFocusPainted(false);
+            cp.add(btnLogin);
+        
+
+        // Event check
+       /* btnLogin.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            checkLogin();  
+            }
+        }); */
+
+        
+        btnLogin.addActionListener(this);
+        txtPassword.addActionListener(this);
+        this.setSize(400,400);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private boolean checkLogin() {
+    String user = txtUser.getText().trim();
+    String pass = new String(txtPassword.getPassword()).trim();
+    return "1".equals(user) && "123".equals(pass);
+    }
+
+
+    private void doLogin() {
+    String user = txtUser.getText().trim();
+    String pass = new String(txtPassword.getPassword()).trim();
+
+    if (user.isEmpty() || pass.isEmpty()) {
+        JOptionPane.showMessageDialog(this, "Please input Username and Password",
+                "Warning", JOptionPane.WARNING_MESSAGE);
+        return;
+    }
+
+    if (checkLogin()) {
+        JOptionPane.showMessageDialog(this, "Login successful!",
+                "Success", JOptionPane.INFORMATION_MESSAGE);
+        SwingUtilities.invokeLater(() -> {
+            new dashboard();  // เปิดหน้าใหม่
+        });
+        dispose();            // ปิด login หลังเปิดหน้าใหม่
+    } else {
+        JOptionPane.showMessageDialog(this, "Username or Password is incorrect",
+                "Error", JOptionPane.ERROR_MESSAGE);
+    }
+}
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==btnLogin || e.getSource()==txtPassword){
+                doLogin();
+        }
+    }
+    
+    
+}
+
