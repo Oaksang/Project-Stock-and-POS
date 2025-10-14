d8f9ccc44f01ac73450d030f8127ca836b0955af
jutamas6721601052 <jutamas.som@ku.th>
2025-10-14 21:36:52 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import UI.*;
-public class Main {
-    public static void main(String[] args) {
-        new loginpanel();
-    }
-}
-//ปรับสีUI
-//เปลี่ยนเป็นjpanelแล้วใช้carglayout+refeash
-//อ่านโค้ดทั้งหมด(ของตัวเอง)
